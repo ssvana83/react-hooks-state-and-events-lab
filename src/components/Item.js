c135c347f1d4c065d5d6bca18893c69a2e5a69b2
Stacy Vana <98608384+ssvana83@users.noreply.github.com>
2022-11-14 10:08:24 -0500

    Done.

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,13 +1,27 @@
-import React from "react";
+import React, {useState} from "react";
+
 
 function Item({ name, category }) {
+  const [addToCart, setAddToCart] = useState(false);
+  const itemClass = addToCart ? "in-cart" : ""
+
+  const handleCart = () => {
+    setAddToCart(!addToCart)
+  }
+
+
   return (
-    <li className="">
+    <li className={itemClass}>
       <span>{name}</span>
       <span className="category">{category}</span>
-      <button className="add">Add to Cart</button>
+      <button onClick={handleCart} className="add"> {addToCart ? "Remove From Cart" : "Add To Cart"}</button>
     </li>
   );
 }
 
 export default Item;
+
+// create function to change the state to t/f
+// then set a condition
+// need to toggle it on button click
+// need to import state on the item component
--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -1,11 +1,23 @@
-import React from "react";
+import React, {useState} from "react";
 import Item from "./Item";
 
 function ShoppingList({ items }) {
+
+  const [category,selectedCategory] = useState();
+
+  const handleSelect = () => {
+    selectedCategory(!category)
+  }
+
+  // const handleSelect(event) {
+  //   selectedCategory(event.target.value)
+  // }
+
+
   return (
     <div className="ShoppingList">
       <div className="Filter">
-        <select name="filter">
+        <select name="filter" onChange={handleSelect}>
           <option value="All">Filter by category</option>
           <option value="Produce">Produce</option>
           <option value="Dairy">Dairy</option>
